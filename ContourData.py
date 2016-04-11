2980e094a898e3909bf1bee762fa810d7509b56a
Cheng Zhang <czhang0328@gmail.com>
2016-04-11 14:14:32 -0400

    modify foldername

--- ContourData.py
+++ ContourData.py
@@ -52,18 +52,57 @@ def build_graph(roadNet):
         
 def read_graph():
     with open(graphJson,'r') as infile:
-        return json.load(infile)
+        graph = json.load(infile)
+        for key in graph:
+            graph[int(key)] = graph.pop(key)
+        return graph
+    
                 
 def find_source_roads(roadNet):
     p = Point(120.14983713626863,30.275047006544927)
     c = p.buffer(0.0001).boundary
+    result = []
     for key,value in roadNet.iteritems():
         l = LineString(value)
         if c.intersects(l):
-            print key,value
+            result.append(key)
+    return result
+
+def day_to_weekday(day, firstWeekDay):
+    # return the weekday num (Sun-0, Mon-1 ...) from the day input
+    # firstWeekDay is 1st day weekday num, e.g. Jan.1st is Saturday, firstWeekDay = 6
+    return (day + firstWeekDay - 1) % 7
+
+def load_speed(day, time):
+    # loadData from one day one hour
+    result = {}
+    fileName = "%s/%s/%s.graph.tarveltime.node.csv" \
+                % (outputDir,day,time)
+    f = open(fileName,'r')
+    count = 0
+    for line in f:
+        line = line.strip().split(' ')
+        if line[0] not in data:
+            result[line[0]] = {'distance':float(line[1]), 'speeds':[], 'speedsWeekdays':[]}
+        tempSpeedsToAdd = [float(x) for x in line[2:] if float(x) >= minAcceptedSpeed]
+        data[line[0]]['speeds'] += tempSpeedsToAdd
+        data[line[0]]['speedsWeekdays'] += [dayToWeekday(day, 6)] * len(tempSpeedsToAdd)
+        count += 1
+        if count > 10 and TEST: break
+    #print "Load %i Data From %s" % (count, fileName)
+    
     
 if __name__ == "__main__":
-    roadNet = read_roadNet()
-    find_source_roads(roadNet)
+    graph = read_graph()
+    vertexes = read_roadNet()
+    sources = find_source_roads(roadNet)
     print("--- %.3f seconds ---" % (time.time() - start_time))
+    
+    
+    
+
+
+
+
+
     
